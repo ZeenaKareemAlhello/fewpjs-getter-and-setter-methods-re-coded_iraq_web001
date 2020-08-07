513770a826412d667fb3ad5ed894f68e583a6a0b
zeena kareem <zena34269@gmail.com>
2020-08-07 12:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,26 @@
 class Circle{
    constructor(radius) {
     this.radius = radius;
-    this.pi=Math.pi;
+   
   }
   get diameter(){
-    return 
+    return this.radius*2
   }
   get circumference(){
-    
+    return Math.pi*this._diameter
   }
   get area(){
-    
+    return Math.pi * radius*radius
+  }
+  
+   set diameter(diameter){
+   this._diameter=diameter;
+  }
+  set circumference(circumference){
+    this._circumference=circumference;
+  }
+  set area(area){
+    this._area=area;
   }
 }
 
